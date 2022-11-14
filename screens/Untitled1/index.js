1d5072572ae1ca9ec162d7969e1d0b25d440be41
Firstjvqglygmmd Lastqmyibincpc <automation3@crowdbotics.com>
2022-11-14 07:41:20 +0000

    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled1/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled1 = () => {
+  return <View style={_styles.AwcrJkmd}>
+      </View>;
+};
+
+export default Untitled1;
+
+const _styles = StyleSheet.create({
+  AwcrJkmd: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});
